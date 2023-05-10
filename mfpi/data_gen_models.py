b4f1e1305fb73f8348a6c40ba41ed5d9173cdbf2
Matteo Sesia <sesia@marshall.usc.edu>
2023-05-10 17:12:01 +0200

    Added first notebook

---
+++ mfpi/data_gen_models.py
@@ -0,0 +1,151 @@
+import numpy as np
+from scipy.stats import norm
+
+class Model_Reg1:
+    def __init__(self, a=0):
+        self.a = a
+        self.sigma = 0.2
+
+    def sample_X(self, n):
+        X = np.random.uniform(0, 1, size=n)
+        X = X.reshape((n,1))
+        return X.astype(np.float32)
+
+    def sample_Y(self, X):
+        x = X[:,0]
+        epsilon = self.sigma * np.random.randn(x.shape[0]) 
+        Y =  x + epsilon        
+        return Y.astype(np.float32).flatten()
+
+    def sample(self, n):
+        X = self.sample_X(n)
+        Y = self.sample_Y(X)
+        return X, Y
+    
+    def oracle_predict(self, X, alpha):
+        x = X[:,0]
+        n = len(x)
+        mu = x
+        sigma = self.sigma
+        lower = norm.ppf(alpha/2, loc=mu, scale=sigma)
+        upper = norm.ppf(1.0-alpha/2, loc=mu, scale=sigma)
+        return lower, upper
+
+
+class Model_Reg2:
+    def __init__(self, a=0):
+        self.a = a
+        self.sigma = 0.2
+
+    def sample_X(self, n):
+        X = np.random.uniform(0, 1, size=n)
+        X = X.reshape((n,1))
+        return X.astype(np.float32)
+
+    def sample_Y(self, X):
+        x = X[:,0]
+        epsilon = self.sigma * (x**2) * np.random.randn(x.shape[0]) 
+        Y =  x + epsilon        
+        return Y.astype(np.float32).flatten()
+
+    def sample(self, n):
+        X = self.sample_X(n)
+        Y = self.sample_Y(X)
+        return X, Y
+    
+    def oracle_predict(self, X, alpha):
+        x = X[:,0]
+        n = len(x)
+        mu = x
+        sigma = self.sigma * (x**2)
+        lower = norm.ppf(alpha/2, loc=mu, scale=sigma)
+        upper = norm.ppf(1.0-alpha/2, loc=mu, scale=sigma)
+        return lower, upper
+
+class Model_Reg3:
+    def __init__(self, a=0):
+        self.a = a
+        self.sigma = 0.1
+        self.df = 2
+
+    def sample_X(self, n):
+        X = np.random.uniform(0, 1, size=n)
+        X = X.reshape((n,1))
+        return X.astype(np.float32)
+
+    def sample_Y(self, X):
+        x = X[:,0]
+        epsilon = self.sigma * np.random.standard_t(df=self.df, size=x.shape[0])
+        Y =  x + epsilon        
+        return Y.astype(np.float32).flatten()
+
+    def sample(self, n):
+        X = self.sample_X(n)
+        Y = self.sample_Y(X)
+        return X, Y
+    
+    def oracle_predict(self, X, alpha):
+        x = X[:,0]
+        n = len(x)
+        mu = x
+        sigma = self.sigma
+        lower = norm.ppf(alpha/2, loc=mu, scale=sigma)
+        upper = norm.ppf(1.0-alpha/2, loc=mu, scale=sigma)
+        return lower, upper
+
+class Model_Reg4:
+    def __init__(self, a=0):
+        self.a = a
+
+    def sample_X(self, n):
+        X = np.random.uniform(0, 1, size=n)
+        X = X.reshape((n,1))
+        return X.astype(np.float32)
+
+    def sample_Y(self, X):
+        x = X[:,0]
+        epsilon = ((1.0-self.a) + 10*self.a*x**2) * np.random.randn(x.shape[0]) 
+        Y =  1 * np.sin(4 * np.pi * x) + 0.25 * epsilon        
+        return Y.astype(np.float32).flatten()
+
+    def sample(self, n):
+        X = self.sample_X(n)
+        Y = self.sample_Y(X)
+        return X, Y
+    
+    def oracle_predict(self, X, alpha):
+        x = X[:,0]
+        n = len(x)
+        mu = 1 * np.sin(4 * np.pi * x)
+        sigma = np.sqrt(0.25 * ((1.0-self.a) + 10*self.a*x**2))
+        lower = norm.ppf(alpha/2, loc=mu, scale=sigma)
+        upper = norm.ppf(1.0-alpha/2, loc=mu, scale=sigma)
+        return lower, upper
+    
+class Model_Class1:
+    def __init__(self, K, p, magnitude=1):
+        self.K = K
+        self.p = p
+        self.magnitude = magnitude
+        # Generate model parameters
+        self.beta_Z = self.magnitude*np.random.randn(self.p,self.K)
+
+    def sample_X(self, n):
+        X = np.random.normal(0, 1, (n,self.p))
+        factor = 0.2
+        X[0:int(n*factor),0] = 1
+        X[int(n*factor):,0] = -8
+        return X.astype(np.float32)
+    
+    def compute_prob(self, X):
+        f = np.matmul(X,self.beta_Z)
+        prob = np.exp(f)
+        prob_y = prob / np.expand_dims(np.sum(prob,1),1)
+        return prob_y
+
+    def sample_Y(self, X):
+        prob_y = self.compute_prob(X)
+        g = np.array([np.random.multinomial(1,prob_y[i]) for i in range(X.shape[0])], dtype = float)
+        classes_id = np.arange(self.K)
+        y = np.array([np.dot(g[i],classes_id) for i in range(X.shape[0])], dtype = int)
+        return y.astype(np.int)
---
+++ mfpi/qr_models.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.stats import norm
+import statsmodels.api as sm
+from sklearn.linear_model import QuantileRegressor
+from quantile_forest import RandomForestQuantileRegressor
+
+class LinearQR:
+    def __init__(self, alpha=0.1):
+        self.alpha = alpha
+        self.qr_low = QuantileRegressor(quantile=alpha/2, alpha=0, solver='highs')
+        self.qr_upp = QuantileRegressor(quantile=1-alpha/2, alpha=0, solver='highs')
+
+    def fit(self, X, Y):
+        self.qr_low.fit(X,Y)
+        self.qr_upp.fit(X,Y)
+
+    def predict(self, X):
+        lower = self.qr_low.predict(X)
+        upper = self.qr_upp.predict(X)
+        # Correct any possible quantile crossings
+        pred = np.concatenate([lower.reshape(len(lower),1), upper.reshape(len(upper),1)],1)
+        pred = np.sort(pred,1)
+        lower = pred[:,0]
+        upper = pred[:,1]
+        return lower, upper
+
+
+class RFQR:
+    def __init__(self, alpha=0.1, min_samples_split=10):
+        self.alpha = alpha
+        self.qr = RandomForestQuantileRegressor(min_samples_split=min_samples_split, min_samples_leaf=5)
+        self.qr_upp = QuantileRegressor(quantile=1-alpha/2, alpha=0, solver='highs')
+
+    def fit(self, X, Y):
+        self.qr.fit(X, Y)
+
+    def predict(self, X):
+        pred = self.qr.predict(X, quantiles=[self.alpha/2, 1-self.alpha/2])
+        lower = pred[:,0]
+        upper = pred[:,1]
+        # Correct any possible quantile crossings
+        pred = np.concatenate([lower.reshape(len(lower),1), upper.reshape(len(upper),1)],1)
+        pred = np.sort(pred,1)
+        return lower, upper
